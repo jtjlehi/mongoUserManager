ed5cb2020b75db011905d587241cd11f264c3712
jared <jared.jacobson.2000@gmail.com>
2018-04-28 15:39:45 -0600

    implemented the edit user method.

--- db_manager.js
+++ db_manager.js
@@ -35,8 +35,39 @@ class DbManager {
             return message;
         })
     }
-    editUser(userID, property, value) {
+    editUser(userID, updateObj) {
         // returns a promise with either a success or error
+        return this._connect()
+        .then(() => {
+            // check if the userID is the correct length, and cast it to ObjectId
+            if (userID.length !== 24) throw new Error('User ID isn\'t the right length');
+            const uid = mongoose.Types.ObjectId(userID);
+            // search for user by id
+            return this.User.findById(uid);
+        })
+        .then((user) => {
+            if (!user) throw new Error('user not found');
+            // reference to old user json for comparison
+            const oldUser = JSON.stringify(user.toObject());
+            user.set(updateObj)
+            // reference to new user json for comparison
+            const userJSON = JSON.stringify(user.toObject());
+            // compare if the old user and new user are the same
+            if (userJSON == oldUser) throw new Error('No change was made');
+            // asynchronously save the new user.
+            return user.save();
+        })
+        // log error on server so I can see it.
+        .catch((err) => {
+            console.log('an error occurred: ', err);
+            mongoose.disconnect();
+            throw err;
+        })
+        // handle error by sending it to the function consumer
+        .then((user) => {
+            mongoose.disconnect();
+            return user;
+        })
     }
     deleteUser(userID) {
         // returns a promise with either a success or error
@@ -44,9 +75,7 @@ class DbManager {
     parse(sortObj) {
         // returns a promise with all of the users or error
         return this._connect()
-            .then(() => this.User.find({})
-                .sort(sortObj)
-            )
+            .then(() => this.User.find({}).sort(sortObj))
             .then((users) => {
                 mongoose.disconnect();
                 return users;
@@ -69,13 +98,14 @@ const dbManager = new DbManager();
 // .catch((err) => {
 //     console.log(err)
 // });
-dbManager.addUser({userName: 'jtjlehi', email: 'jtjlehi@gmail.com', age: 6})
-.then(success => {
-    console.log(success);
-})
+// dbManager.addUser({userName: 'jtjlehi', email: 'jtjlehi@gmail.com', age: 6})
+// .then(success => {
+//     console.log(success);
+// })
 // .catch(err => {
 //     console.log('error has occurred');
 //     console.log(err);
 // })
 // .then(() => dbManager.parse({age: 1}))
 // .then((users) => {console.log(users)});
+dbManager.editUser('5ae4d8272b0e69d0dbee62e7', {userName: 'jtjlehi'});
