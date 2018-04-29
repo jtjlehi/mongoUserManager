9e2cd9cd9165ffa58c304d1778e4780f305f680b
jared <jared.jacobson.2000@gmail.com>
2018-04-28 21:23:56 -0600

    made app_post user dbManager

--- add_post.js
+++ add_post.js
@@ -1,12 +1,13 @@
-const DataManager = require('./data_manager');
+const dbManager = require('./db_manager');
 
 module.exports = function(app) {
     app.post('/add', (req, res) => {
-        const user = DataManager.generateUser(req.body.name, req.body.email, req.body.age);
-        DataManager.addUser(user)
-        .then((err) => {
-            if (err) res.render('message', {message: err.message});
-            res.render('message', {message: 'added User'})
+        dbManager.addUser({userName: req.body.name, email: req.body.email, age: req.body.age})
+        .then((message) => {
+            res.render('message',  {message: 'Added new user. Return home to view them.'});
+        })
+        .catch(err => {
+            res.render('message', err);
         })
     });
 }
